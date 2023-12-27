4a713c510eedb272b29ac6f3f276a2fb7b05ec9d
Thomas Wang <wang.thomasdavid@gmail.com>
2023-12-27 14:06:13 -0500

    Initial Commit

---
+++ Quest-Twitch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quest-Twitch
+//
+//  Created by Thomas Wang on 12/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quest-Twitch/Quest_TwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quest_TwitchApp.swift
+//  Quest-Twitch
+//
+//  Created by Thomas Wang on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Quest_TwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
